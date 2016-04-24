2bc05372687dd247d599482797fe6f58c733ad98
artur99 <david1989mail@yahoo.com>
2016-04-24 20:35:27 +0300

    updated db structure

--- db_structure.sql
+++ db_structure.sql
@@ -3,12 +3,13 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 22, 2016 at 04:22 PM
+-- Generation Time: Apr 24, 2016 at 07:34 PM
 -- Server version: 10.0.17-MariaDB-log
 -- PHP Version: 5.6.14
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -46,11 +47,27 @@ CREATE TABLE `periods` (
   `id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `location` text NOT NULL,
+  `description` text NOT NULL,
   `sdate` varchar(8) NOT NULL,
   `stime` varchar(6) NOT NULL,
   `edate` varchar(8) NOT NULL,
   `etime` varchar(6) NOT NULL,
   `color` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todo`
+--
+
+CREATE TABLE `todo` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `text` text NOT NULL,
+  `status` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -92,6 +109,13 @@ ALTER TABLE `periods`
   ADD KEY `event_id` (`event_id`);
 
 --
+-- Indexes for table `todo`
+--
+ALTER TABLE `todo`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -112,6 +136,11 @@ ALTER TABLE `events`
 -- AUTO_INCREMENT for table `periods`
 --
 ALTER TABLE `periods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `todo`
+--
+ALTER TABLE `todo`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `users`
